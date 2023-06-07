3ec9c1639335cac7ee89f02240355d8d088bd5b5
Kirill Chernyy <kirill070@gmail.com>
2023-06-07 14:32:29 +0300

    Make brain-gcd game

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 <a href="https://codeclimate.com/github/Kirill070/php-project-45/maintainability"><img src="https://api.codeclimate.com/v1/badges/bf304ab1a22cc90bb35e/maintainability" /></a><br>
 [Запись примера игры "Проверка на четность"](https://asciinema.org/a/kcDHNqyWRjF3UcjGLKErHaCjO)<br>
 [Запись примера игры "Калькулятор"](https://asciinema.org/a/KlJJM48p5h9rGgza1kZNBfwph)<br>
+[Запись примера игры "НОД"](https://asciinema.org/a/2Nk5RC2QLCk0FGdWWCpkposXM)<br>
---
+++ src/Games/Gcd.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Hexlet\Code\Gcd;
+
+use function cli\line;
+use function cli\prompt;
+use function Hexlet\Code\Engine\greeting;
+use function Hexlet\Code\Engine\playGame;
+
+function makeTask(): array
+{
+    $num1 = rand(1, 99);
+    $num2 = rand(1, 99);
+
+    $question = "{$num1} {$num2}";
+    while ($num1 !== $num2) {
+        if ($num1 > $num2) {
+            $num1 -= $num2;
+        } else {
+            $num2 -= $num1;
+        }
+    }
+    $trueAnswer = $num1;
+
+    $trueAnswer = (string) $trueAnswer;
+    $result = [$question, $trueAnswer];
+
+    return $result;
+}
+
+function runGameGcd()
+{
+    $description = 'Find the greatest common divisor of given numbers.';
+
+    $name = greeting($description);
+
+    for ($i = 0; $i < 3; $i++) {
+        $task = makeTask();
+        $result = playGame($name, $task);
+        if ($result === true) {
+            $result = 'Congratulations, ' . $name;
+        } else {
+            $result = "Let's try again, " . $name;
+            break;
+        }
+    }
+
+    line($result);
+}
